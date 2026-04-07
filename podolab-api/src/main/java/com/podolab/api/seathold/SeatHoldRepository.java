7cea9ad3194daf02360d30373e79834bbb7f68c7
u-genuine <u_genuine@naver.com>
2026-04-07 18:08:56 +0900

    [REMOVE] SeatHold 관련 코드 전체 제거 #24

--- podolab-api/src/main/java/com/podolab/api/seathold/SeatHoldRepository.java
+++
@@ -1,7 +0,0 @@
-package com.podolab.api.seathold;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatHoldRepository extends JpaRepository<SeatHold, Long> {
-	int countBySeatId(Long seatId);
-}
